13aeb4f27159b14785f413fa0b1b9c25ce2b01c9
mmorrons <158231647+mmorrons@users.noreply.github.com>
2025-03-11 21:38:05 +0100

    Update ojdata.py

--- ojdata.py
+++ ojdata.py
@@ -1,4 +1,10 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title="SAS - AUC", 
+    page_icon="🏃",
+    layout="wide")
+
 import xml.etree.ElementTree as ET
 import pandas as pd
 from io import BytesIO
@@ -159,6 +165,7 @@ def process_single_file(file_bytes, original_filename):
 
 def main():
     st.title("Optojump Data Analyser")
+    st.write("Seleziona i file di optojump, attenta che siano descritti come Cognome_Nome(o_più_nomi)_Treadmill_8km_h_28_02_2025_13_18_12_Sessione_Musica/Nomusica
 
     # Let the user upload multiple XML files
     uploaded_files = st.file_uploader(
